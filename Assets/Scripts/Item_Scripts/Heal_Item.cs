77f01b8ce33cddfd33308592c960bb73db93eac8
Geunhyung Song <gamyeonx@gmail.com>
2026-01-27 13:05:49 +0900

    [ADD] 체력, 탄창 아이템 프리팹 및 스크립트 추가
    현재 플레이어 데이터가 없어서, 임시로 구상만 하고, 이후에 수정예정

---
+++ Assets/Scripts/Item_Scripts/Heal_Item.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Heal_Item : MonoBehaviour
+{
+    [SerializeField] private int _healingValue;
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (!other.CompareTag("Player")) return;
+
+    //    플레이어능력치 player = other.GetComponent<플레이어능력치>();
+
+    //    if (player == null) return;
+
+    //    player.플레이어채력 += _healingValue;
+
+    //    if (player.플레이어채력 > player.플레이어최대채력)
+    //    {
+    //        player.플레이어채력 = player.플레이어최대채력;
+    //    }
+
+    //    Destroy(gameObject);
+    //}
+}
