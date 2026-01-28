e1181f7a83474cbd79592450b93ff15d2d24f00d
kamyou <example1130@naver.com>
2026-01-28 16:29:43 +0900

    [ADD] Bomb Function 스크립트 및 기능 생성

---
+++ Assets/Scripts/BombFunction.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombFunction : MonoBehaviour
+{
+    private int _health;
+
+    public int Health
+    {
+        get => _health;
+        set
+        {
+            _health = value;
+        }
+    }
+    
+    [SerializeField] [Range(0, 10)] private float _explosionRange;
+    private bool _isExploded = false;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ActiveBomb();
+        }
+
+        if (_isExploded)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    private void ActiveBomb()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, _explosionRange);
+
+        foreach (Collider hit in hitColliders)
+        {
+            MonsterController monster = hit.GetComponent<MonsterController>();
+
+            if (monster != null)
+            {
+                Debug.Log($"{monster.gameObject.name} 데미지 입힘");
+            }
+        }
+        
+        _isExploded = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _explosionRange);
+    }
+}
