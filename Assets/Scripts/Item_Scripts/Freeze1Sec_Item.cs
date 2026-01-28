f0e7714516793c84e970baf767ef1e36987d45d9
Geunhyung Song <gamyeonx@gmail.com>
2026-01-28 15:19:56 +0900

    [ADD] AmmoCabinet 스크립트 추가
    플레이어의 탄창수를 따로 회복할 수단은 있어야 한다고 판다.
    몬스터처럼 시간당 AmmoCabinet이 없다는 가정하에 배치되는 식이면 좋을 것 같아서 임시로 제작해 놨습니다.

--- Assets/Scripts/Item_Scripts/Freeze1Sec_Item.cs
+++ Assets/Scripts/Item_Scripts/Freeze1Sec_Item.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class Freeze1Sec_Item : MonoBehaviour
 {
-    [Tooltip("이동기능 스크립트를 추가해 주세요.")]
-    [SerializeField] private CharacterMovement _player;
+    //[Tooltip("이동기능 스크립트를 추가해 주세요.")]
+    //[SerializeField] private CharacterMovement _player;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (!other.CompareTag("Player")) return;
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (!other.CompareTag("Player")) return;
 
-        if (_player.enabled == true)
-        {
-            StartCoroutine(Freeze1SecMove());
-        }
-    }
+    //    if (_player.enabled == true)
+    //    {
+    //        StartCoroutine(Freeze1SecMove());
+    //    }
+    //}
 
-    private IEnumerator Freeze1SecMove()
-    {
-        _player.enabled = false;
-        yield return new WaitForSeconds(1f);
-        _player.enabled = true;
-        Destroy(gameObject);
-    }
+    //private IEnumerator Freeze1SecMove()
+    //{
+    //    _player.enabled = false;
+    //    yield return new WaitForSeconds(1f);
+    //    _player.enabled = true;
+    //    Destroy(gameObject);
+    //}
 }
