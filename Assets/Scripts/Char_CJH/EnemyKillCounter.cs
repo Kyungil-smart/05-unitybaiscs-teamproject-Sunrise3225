06ff03a44109a1bc3655f90d44ac27fca517178f
Blind-ness-777 <wlsgod10@gmail.com>
2026-01-28 09:40:41 +0900

    [REFACTOR] KillCounter Public으로 전환

--- Assets/Scripts/Char_CJH/EnemyKillCounter.cs
+++ Assets/Scripts/Char_CJH/EnemyKillCounter.cs
@@ -6,7 +6,8 @@ public class EnemyKillCounter : MonoBehaviour
 {
     private float _killCount;
 
-    private void KillCounter()
+    // TODO 적 처치 시 KillCounter 메서드 추가해 주세요.
+    public void KillCounter()
     {
         _killCount++;
         Debug.Log($"처치 수 = {_killCount}");
