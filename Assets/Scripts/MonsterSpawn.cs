de2ae941e2c9fc1cf28e2c2ccd41fae50a799655
kamyou <example1130@naver.com>
2026-01-29 11:06:03 +0900

    [UPDATE] Monster Spawn 스크립트 Wave 기능 추가

--- Assets/Scripts/MonsterSpawn.cs
+++ Assets/Scripts/MonsterSpawn.cs
@@ -6,12 +6,13 @@ using Random = UnityEngine.Random;
 
 public class MonsterSpawn : MonoBehaviour
 {
+    // 몬스터 프리팹의 종류와 스폰 위치를 설정하는 리스트
     [SerializeField] private List<GameObject> _spawnPrefabs = new List<GameObject>();
     [SerializeField] private List<Vector3> _spawnPositions = new List<Vector3>();
     
     // 코루틴 접근 필드
+    [SerializeField] private float _waveDelay;
     [SerializeField] private float _spawnDelay;
-    [SerializeField] private float _spawnInterval;
     
     // 생성 위치를 랜덤으로 설정하기 위한 인덱스
     private int _randPositionSelect; 
@@ -21,9 +22,18 @@ public class MonsterSpawn : MonoBehaviour
     private int _randMonsterSelect; 
     private GameObject _spawnPrefab;
     
-    // 최대 생성 몬스터를 설정하기 위한 필드
+    // 첫 웨이브 최대 생성 몬스터를 설정하기 위한 필드
+    // 웨이브마다 몬스터의 수가 1.5배 증가
     [SerializeField] private int _maxSpawnCount;
     private int _spawnCount;
+
+    private int _aliveMonsterCount;
+
+    // 최대 웨이브 수를 결정하는 필드
+    [SerializeField] private int _maxWaveCount;
+    private int _waveCount;
+
+    private float _timer;
 
     private void Awake()
     {
@@ -32,12 +42,17 @@ public class MonsterSpawn : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(Spawn());
+        WaveStart();
     }
 
     private void Update()
     {
         RandomSpawn();
+
+        if (_maxWaveCount < _waveCount)
+        {
+            WaveStop();
+        }
     }
 
     private void Init()
@@ -54,14 +69,50 @@ public class MonsterSpawn : MonoBehaviour
         _spawnPrefab = _spawnPrefabs[_randMonsterSelect];
     }
 
+    private void WaveStart()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private void WaveStop()
+    {
+        StopCoroutine(Spawn());
+    }
+    
     private IEnumerator Spawn()
     {
-        while (_spawnCount < _maxSpawnCount)
+        while (_waveCount < _maxWaveCount)
         {
-            Instantiate(_spawnPrefab, _spawnPosition, Quaternion.identity);
-            _spawnCount++;
-            yield return new WaitForSeconds(_spawnDelay);
+            _waveCount++;
+            
+            while (_spawnCount < _maxSpawnCount)
+            {
+                SpawnMonster();
+                _spawnCount++;
+                yield return new WaitForSeconds(_spawnDelay);
+            }
+
+            yield return new WaitUntil(() => _aliveMonsterCount <= 0);
+        
+            _maxSpawnCount = (int)(_maxSpawnCount * 1.5f);
+            _spawnCount = 0;
+            yield return new WaitForSeconds(_waveDelay);
         }
+    }
+
+    private void SpawnMonster()
+    {
+        GameObject monster = Instantiate(_spawnPrefab, _spawnPosition, Quaternion.identity);
+        _aliveMonsterCount++;
+        Debug.Log($"생성된 몬스터 수 : {_aliveMonsterCount}");
+
+        monster.GetComponent<TestMons>().Init(this);
+    }
+
+    public void OnMonsterSpawn()
+    {
+        _aliveMonsterCount--;
+        Debug.Log($" 현재 남은 몬스터 수 : {_aliveMonsterCount}");
     }
 
     private void OnDrawGizmos()
