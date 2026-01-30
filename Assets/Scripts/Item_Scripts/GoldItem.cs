dcb12c588c8d8dd25a6e19b3f5b5de04d2691c49
Geunhyung Song <gamyeonx@gmail.com>
2026-01-30 19:43:16 +0900

    [ADD] GoldItem 스크립트 추가
    함수를 만들어도 병합과정에서 사라지는 현상이 있고, 골드 관련 UI도 현시점에서는 없기에 총알로 테스트

---
+++ Assets/Scripts/Item_Scripts/GoldItem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldItem : MonoBehaviour
+{
+    [Tooltip("°ñµå È¹µæ·®")]
+    [SerializeField] private int _moneyValue;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        CharacterController gold = other.GetComponent<CharacterController>();
+
+        if (gold == null) return;
+
+        gold.RefillAmmo(_moneyValue);
+        Destroy(gameObject);
+    }
+
+
+}
